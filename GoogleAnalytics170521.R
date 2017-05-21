ababfa41e4d37d97287b7627c07b7ef6272fb271
talonendm <talonen.dm@gmail.com>
2017-05-21 05:52:30 +0300

    170521 Samsung AnalyticsR fixed

---
+++ GoogleAnalytics170521.R
@@ -0,0 +1,15 @@
+# libraries to default lib:
+.libPaths()
+# install.packages('googleAnalyticsR')
+
+
+# install.packages('curl')
+# http://stackoverflow.com/questions/20671814/non-zero-exit-status-r-3-0-1-xml-and-rcurl
+# in terminal: sudo apt-get install libcurl4-openssl-dev libxml2-dev
+# install.packages('httr')
+
+library(googleAnalyticsR)
+
+
+
+
